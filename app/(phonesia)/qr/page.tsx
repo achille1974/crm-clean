5061e0df796d1e676287436feed6c8495744da6b
Achille Beltrami <achille.beltrami@gmail.com>
2026-01-12 19:33:11 +0100

    QrPage requires negozio param (QR per negozio)

--- app/(phonesia)/qr/page.tsx
+++ app/(phonesia)/qr/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import QrForm from "@/components/phonesia/qr/QrForm";
 
 export default function QrPage() {
-  return <QrForm />;
+  const searchParams = useSearchParams();
+  const negozio = searchParams.get("negozio");
+
+  if (!negozio) {
+    return (
+      <p style={{ padding: 20, fontWeight: 700 }}>
+        QR non valido: negozio mancante
+      </p>
+    );
+  }
+
+  return <QrForm negozioId={Number(negozio)} />;
 }
